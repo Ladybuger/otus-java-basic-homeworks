b65191475428a804e3e4d5904daca7e3b4e40ad1
Nikitina Nataliya <nekkitina@gmail.com>
2024-11-09 20:10:18 +0300

    HW4: work on mistakes1: Box.java, MainApplication.java, User.java

--- src/main/java/ru/otus/java/basic/homeworks/homework4/Box.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/Box.java
@@ -9,23 +9,23 @@ public class Box {
     public int getLength() {
         return length;
     }
-    public void setLength(int length) {
+    /*public void setLength(int length) {
         this.length = length;
-    }
+    }*/
 
     public int getWidth() {
         return width;
     }
-    public void setWidth(int width) {
+    /*public void setWidth(int width) {
         this.width = width;
-    }
+    }*/
 
     public int getHeight() {
         return height;
     }
-    public void setHeight(int height) {
+    /*public void setHeight(int height) {
         this.height = height;
-    }
+    }*/
 
     public String getColor() { return color; }
     public void setColor(String color) { this.color = color; }
