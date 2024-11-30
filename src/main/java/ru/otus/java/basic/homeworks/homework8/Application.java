2b6e75a072562c1fb6119ba5bd2bdbf68bb21aed
Nikitina Nataliya <nekkitina@gmail.com>
2024-11-30 22:44:27 +0300

    HW8

--- src/main/java/ru/otus/java/basic/homeworks/homework8/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework8/Application.java
@@ -1,32 +1,53 @@
 package ru.otus.java.basic.homeworks.homework8;
 
 public class Application {
-    public static void processArray(String[][] array) {
-        try {
-            if (array.length != 4 || array[0].length != 4) {
-                throw new AppArraySizeException("Array size must be 4x4");
-            }
-
-        } catch (AppArraySizeException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
     public static void main(String[] args) {
         String[][] array = {
-                {"a", "b", "c", "d"},
-                {"e", "f", "g", "h"},
-                {"i", "j", "k", "l"},
-                {"m", "n", "o", "p"}
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
         };
 
-        processArray(array);
+        try {
+            int sum = sumArrayElements(array);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (AppArraySizeException e) {
+            System.err.println("Исключение: " + e.getMessage());
+        } catch (AppArrayDataException e) {
+            System.err.println("Исключение: " + e.getMessage());
+        }
+    }
+
+    public static int sumArrayElements(String[][] array) {
+        if (array.length != 4 || array[0].length != 4 || array[1].length != 4 || array[2].length != 4 || array[3].length != 4) {
+            throw new AppArraySizeException("Неверный размер массива");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new AppArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+
+        return sum;
     }
 }
 
-class AppArraySizeException extends Exception {
+class AppArraySizeException extends RuntimeException {
     public AppArraySizeException(String message) {
         super(message);
     }
+}
 
+class AppArrayDataException extends RuntimeException {
+    public AppArrayDataException(String message) {
+        super(message);
+    }
 }
