93cfe2a8aaca86c0cb8a4ede3dff25335cb5e98c
Nikitina Nataliya <nekkitina@gmail.com>
2024-12-16 20:40:21 +0300

    HW12 замечания

--- src/main/java/ru/otus/java/basic/homeworks/homework12/MainApplication.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework12/MainApplication.java
@@ -15,17 +15,32 @@ public class MainApplication {
             return;
         }
 
+        outputFileList(files);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите полное имя файла с расширением .txt: ");
+        String fileName = scanner.nextLine();
+
+        outputFileLContents(fileName);
+
+        System.out.print("Введите строку для записи в файл: ");
+        String input = scanner.nextLine();
+
+        writeStringToFile(fileName, input);
+
+        scanner.close();
+    }
+
+    public static void outputFileList(File[] files) {
         System.out.println("Список файлов:");
         for (File file : files) {
             if (file.isFile() && file.getName().endsWith(".txt")) {
                 System.out.println(file.getName());
             }
         }
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите полное имя файла с расширением .txt: ");
-        String fileName = scanner.nextLine();
-
+    public static void outputFileLContents(String fileName) {
         try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(fileName))) {
             int n = in.read();
             while (n != -1) {
@@ -35,20 +50,17 @@ public class MainApplication {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        System.out.print("Введите строку для записи в файл: ");
-        String input = scanner.nextLine();
-
+    public static void writeStringToFile(String fileName, String input) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
             if (new File(fileName).length() != 0) {
-                writer.newLine(); // Добавляем переход на новую строку, если файл не пустой
+                writer.newLine();
             }
             writer.write(input);
             System.out.println("Строка успешно записана в файл.");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        scanner.close();
     }
 }
